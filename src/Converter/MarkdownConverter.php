db7c0574352b80f48ad2efe529e2fc0985222f5b
ukatama <dev.ukatama@gmail.com>
2016-03-15 02:01:14 +0900

    Extend php-markdown

--- src/Converter/MarkdownConverter.php
+++ src/Converter/MarkdownConverter.php
@@ -5,60 +5,75 @@ namespace ukatama\Mew\Converter;
 use Michelf\MarkdownExtra;
 use ukatama\Mew\Page;
 
+class MewMarkdown extends MarkdownExtra {
+    static public function pageTransform($text, $page, $pageStack = []) {
+		$parser_class = \get_called_class();
+
+        $parser = new $parser_class;
+        $parser->page = $page;
+        $parser->pageStack = $pageStack;
+		return $parser->transform($text);
+    }
+
+    public function __construct() {
+        $this->span_gamut += [
+            'doAbbrAnchors' => 9, // Before doImages/doAnchors
+            'doFileTransclusion' => 41, // After encodeAmpsAndAngles
+        ];
+
+        parent::__construct();
+    }
+
+    protected function doFileTransclusion($text) {
+        $text = preg_replace_callback('/{{(.*?)}}/', function($matches) {
+            $name = $matches[1];
+            if (!array_key_exists($name, $this->pageStack)) {
+                $page = new Page($name, 'name');
+                return MewMarkdown::pageTransform($page->getHead()->getData(), $name);
+            } else {
+                return $matches[0];
+            }
+        }, $text);
+
+        return $text;
+    }
+
+    protected function doAbbrAnchors($text) {
+        return preg_replace('/\[(.*?)\](?!\()/', '[$1]($1)', $text);
+    }
+
+    protected function _checkPageLink($url) {
+        return !preg_match('/^(https?)?:\/\//', $url);
+    }
+
+    protected function _doAnchors_inline_callback($matches) {
+        $url = $matches[3] == '' ? $matches[4] : $matches[3];
+        if ($this->_checkPageLink($url)) {
+            $encoded = urlencode($url);
+            $matches[3] = "?p=$encoded";
+        }
+        return parent::_doAnchors_inline_callback($matches);
+    }
+
+    protected function _doImages_inline_callback($matches) {
+        $url = $matches[3] == '' ? $matches[4] : $matches[3];
+        if ($this->_checkPageLink($url)) {
+            preg_match('/^((.*)\/)?([^\/]+)$/', $url, $fmatches);
+
+            $page = urlencode($fmatches[2] == '' ? $this->page : $fmatches[2]);
+            $file = $fmatches[3];
+
+            $matches[3] = "?a=file&p=$page&f=$file";
+        }
+        return parent::_doImages_inline_callback($matches);
+    }
+}
+
 class MarkdownConverter extends Converter {
-    private $pageStuck = [];
+    private $pageStack = [];
 
     public function convert($src, $page = '') {
-        $this->pageStuck[$page] = true;
-
-        $src = preg_replace_callback('/((.|\r|\n)*?)((```(.|\r|\n)*?```)|(`(.|\r|\n)*?`)|$)/m', function ($matches) use ($page) {
-            $src = $matches[1];
-
-            // File Transclusion
-            $src = preg_replace_callback('/{{(.*?)}}/', function($matches) use ($page) {
-                $name = $matches[1];
-                if (!array_key_exists($name, $this->pageStuck)) {
-                    $page = new Page($name, 'name');
-                    return $this->convert($page->getHead()->getData(), $name);
-                } else {
-                    return $matches[0];
-                }
-            }, $src);
-
-            // Abbr link
-            $src = preg_replace('/\[(.*?)\](?!\()/', '[$1]($1)', $src);
-            $src = preg_replace_callback('/(!?)\[(.*?)]\((.*?)( .*?)?\)/', function($matches) use ($page) {
-                if (preg_match('/^(https?)?:\/\//', $matches[3])) {
-                    return $matches[0];
-                } else {
-                    if (empty($matches[1])) {
-                        $encoded = urlencode($matches[3]);
-                        if (count($matches) == 5) {
-                            return "[{$matches[2]}](?p=$encoded {$matches[4]})";
-                        } else {
-                            return "[{$matches[2]}](?p=$encoded)";
-                        }
-                    } else {
-                        preg_match('/^((.*)\/)?([^\/]+)$/', $matches[3], $path);
-
-                        if (strlen($path[2]) == 0) {
-                            $page_name = urlencode($page);
-                        } else {
-                            $page_name = urlencode($path[2]);
-                        }
-
-                        $file_name = urlencode($path[3]);
-                        if (count($matches) == 5) {
-                            return "![{$matches[2]}](?a=file&p=$page_name&f=$file_name {$matches[4]})";
-                        } else {
-                            return "![{$matches[2]}](?a=file&p=$page_name&f=$file_name)";
-                        }
-                    }
-                }
-            }, $src);
-
-            return $src . $matches[3];
-        }, $src);
-        return MarkdownExtra::defaultTransform($src);
+        $this->pageStack[$page] = true;
+        return MewMarkdown::pageTransform($src, $page, $this->pageStack);
     }
 }
